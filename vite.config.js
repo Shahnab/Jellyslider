dbfbc32fcd1d874234d96e6c65e8f85ed2a426c9
SHAHNAB AHMED <ahmedshahnab@gmail.com>
2025-11-04 01:34:47 +0700

    Initial commit: Jelly Slider project with WebGPU and TypeGPU

---
+++ vite.config.js
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vite';
+import typegpuPlugin from 'unplugin-typegpu/vite';
+
+export default defineConfig({
+  plugins: [typegpuPlugin()],
+  base: '/Jellyslider/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+  }
+});
